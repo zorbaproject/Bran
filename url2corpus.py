867e51c718326d7168bf4c97e786d6b2c745896c
Luca Tringali <tringalinvent@libero.it>
2018-05-20 19:26:59 +0200

    recursive looking for pages

--- url2corpus.py
+++ url2corpus.py
@@ -8,9 +8,15 @@ import sys
 import os
 
 
-useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-thisurl = "http://www.repubblica.it/esteri/2018/05/18/news/aereo_incidente_schianto_cuba_decollo-196760241/?ref=RHPPLF-BH-I0-C8-P7-S1.8-T1"
+#there are a few urls we should ignore
+ignore = ['quotidiano.repubblica.it', 'rep.repubblica.it', 'trovacinema.repubblica.it', 'miojob.repubblica.it', 'racconta.repubblica.it', 'video.repubblica.it', 'www.repubblica.it/economia/miojob/', 'facebook.com', 'google.com', 'yahoo.com', 'twitter.com', 'ansa.it/games/', 'ansa.it/meteo/', 'ansa.it/nuova_europa/', 'corporate.ansa.it', 'filmalcinema.shtml', 'trovacinema', 'splash.repubblica.it', 'd.repubblica.it']
 
+useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+thisurl = ""
+visited = []
+firstrun = True
+visitedfile = ""
 
 def find_between(s, first, last ):
     try:
@@ -33,32 +39,41 @@ def geturl(thisurl):
             'User-Agent': useragent
         }
     )
-
-    f = urllib.request.urlopen(req)
-    thishtml = f.read().decode('utf-8')
-    thishtml = html.unescape(thishtml)
+    
+    try:
+        f = urllib.request.urlopen(req)
+        thishtml = f.read().decode('utf-8')
+        thishtml = html.unescape(thishtml)
+    except:
+        thishtml = ""
     return thishtml
 
 
 def cleanRepubblica(thishtml):
-    #get only the article
-    start = re.escape('<div class="body-text">')
-    end = re.escape('<div id="fb-facepile">')
-    
-    indexes = [(m.start(0), m.end(0)) for m in re.finditer(start, thishtml)]
+    #look for known article delimiters
+    start = ['<div class="body-text".*?>', '<div class="entrytext".*?>', '<div class="post-entry".*?>', '<div class="entry-content".*?>', '<div class="article-maincolblog".*?>']
+    end = ['<div id="fb-facepile">','<p class="dettagliotag">', '<footer', '<p class="postmetadata">']
+    cs = 0
+    ce = 0
+    for i in range(len(start)):
+        if (bool(re.search(start[i], thishtml))):
+            cs = i
+    for i in range(len(end)):
+        if (bool(re.search(end[i], thishtml))):
+            ce = i
+    #if we didn't find delimitier, this is an unknown article type so we stop here
+    indexes = [(m.start(0), m.end(0)) for m in re.finditer(start[cs], thishtml)]
     if len(indexes)<1:
         return ""
     ns = indexes[0][1]
-    indexes = [(m.start(0), m.end(0)) for m in re.finditer(end, thishtml)]
+    indexes = [(m.start(0), m.end(0)) for m in re.finditer(end[ce], thishtml)]
     if len(indexes)<1:
         return ""
     ne = indexes[0][0]
+    #get only the article
     if ns < 0 or ne < 0:
         return ""
     thishtml = thishtml[ns:ne]
-    
-    #remove until the first mark, because usually the first phrase is useless
-    thishtml = re.sub("<strong>.*?<\/strong>.*?[\.]", "", thishtml)
     
     #remove photogalleries
     start = re.escape('<div class="snappedPlaceholder"')
@@ -77,6 +92,34 @@ def cleanRepubblica(thishtml):
     
     return thishtml
 
+def cleanAnsa(thishtml):
+    #look for known article delimiters
+    start = ['<div itemprop="articleBody".*?>']
+    end = ['<div id="relatedMobile"']
+    cs = 0
+    ce = 0
+    for i in range(len(start)):
+        if (bool(re.search(start[i], thishtml))):
+            cs = i
+    for i in range(len(end)):
+        if (bool(re.search(end[i], thishtml))):
+            ce = i
+    #if we didn't find delimitier, this is an unknown article type so we stop here
+    indexes = [(m.start(0), m.end(0)) for m in re.finditer(start[cs], thishtml)]
+    if len(indexes)<1:
+        return ""
+    ns = indexes[0][1]
+    indexes = [(m.start(0), m.end(0)) for m in re.finditer(end[ce], thishtml)]
+    if len(indexes)<1:
+        return ""
+    ne = indexes[0][0]
+    #get only the article
+    if ns < 0 or ne < 0:
+        return ""
+    thishtml = thishtml[ns:ne]
+    
+    return thishtml
+
 def cleanGeneric(thishtml):
     
     #clean headers 
@@ -86,8 +129,11 @@ def cleanGeneric(thishtml):
     thishtml = re.sub("<a .*?<\/a>", "", thishtml, flags=re.DOTALL)
     
     #clean js and css
-    thishtml = re.sub("<script>.*?<\/script>", "", thishtml, flags=re.DOTALL)
-    thishtml = re.sub("<style>.*?<\/style>", "", thishtml, flags=re.DOTALL)
+    thishtml = re.sub("<script.*?<\/script>", "", thishtml, flags=re.DOTALL)
+    thishtml = re.sub("<style.*?<\/style>", "", thishtml, flags=re.DOTALL)
+    
+    #remove strong tag if in caps lock
+    thishtml = re.sub("<strong>[^a-z]+?<\/strong>", "", thishtml)
     
     #clean all useless symbols
     mysymbols = re.escape('+*#')
@@ -96,11 +142,11 @@ def cleanGeneric(thishtml):
     #clean all tags (NOTE: DOTALL means that . matches every characters including \n)
     thishtml = re.sub("<.*?>", "", thishtml, flags=re.DOTALL)
     
+    #TODO: it might be a good idea to check if at least a few words in every lines belong to the vdb, otherwise we should delete those lines
+    
     #remove all empty lines
     stripped = [line for line in thishtml.split('\n') if line.strip() != '']
     thishtml = "".join(stripped)
-    
-    #TODO: it might be a good idea to check if at least a few words in every lines belong to the vdb, otherwise we should delete those lines
     
     #remove double spaces
     while (bool(re.search('\s\s', thishtml))):
@@ -108,8 +154,18 @@ def cleanGeneric(thishtml):
     
     #remove initial spaces
     thishtml = re.sub("^\s", "", thishtml)
+    thishtml = re.sub("^\s*?-", "", thishtml)
+    thishtml = re.sub("^(di)\s", "", thishtml)
     
     return thishtml
+
+def getLinks(thishtml):
+    links = [m.group(1) for m in re.finditer("<a .*?href=\"(http.*?|\/.*?)\".*?<\/a>", thishtml)]
+    return links
+
+def getRSSLinks(thishtml):
+    links = [m.group(1) for m in re.finditer("<link>.*?(http.*?)(\]\]>)*?<\/link>", thishtml, flags=re.DOTALL)]
+    return links
 
 def url2name(thisurl):
     myname = re.sub(re.escape('http://'), "", thisurl)
@@ -120,9 +176,34 @@ def url2name(thisurl):
     return myname
 
 def runOnPage(thisurl, output = ""):
+    global ignore 
+    
+    firstrun = False
     thishtml = geturl(thisurl)
+    links = getLinks(thishtml)
+    m = re.match(r"(http.*?\..*?)(\/|$)", thisurl)
+    baseurl = m.group(1)
+    for i in range(len(links)):
+        if links[i][0] == '/':
+            links[i] = baseurl+ links[i]
+        for ii in range(len(ignore)):
+            if ignore[ii] in links[i]:
+                links[i] = ''
+    for ii in range(len(ignore)):
+        if ignore[ii] in thisurl:
+            return []
+    #cleaning for Repubblica.it
     if 'repubblica.it' in thisurl:
+        if 'rss2.0.xml' in thisurl:
+            links = getRSSLinks(thishtml)
+            return links
         thishtml = cleanRepubblica(thishtml)
+    #cleaning for ANSA.it
+    if 'ansa.it' in thisurl:
+        if 'rss.xml' in thisurl:
+            links = getRSSLinks(thishtml)
+            return links
+        thishtml = cleanAnsa(thishtml)
     thishtml = cleanGeneric(thishtml)
     if output == "":
         print(thishtml)
@@ -132,26 +213,48 @@ def runOnPage(thisurl, output = ""):
             text_file = open(fname, "w")
             text_file.write(thishtml)
             text_file.close()
+    return links
     
 def runRecursive(thisurl, output = ""):
+    global visited
     #before going on, check if we previously worked on this page
+    m = re.match(r"http.*?\.(.*?)(\/|$)", thisurl)
+    baseurl = m.group(1)
     fname = fname = output + "/" + url2name(thisurl)
-    if os.path.isfile(fname) == False:
-        runOnPage(thisurl, output)
+    if os.path.isfile(fname) == False or firstrun:
+        if re.sub("\?.*$", "", thisurl) not in visited:
+            links = runOnPage(thisurl, output)
+            visited.append(thisurl)
+        else:
+            links = []
         #look for every link in the page with the same starting url (up to the first /)
-        #before goin on, check if we previously worked on this page
+        for i in range(len(links)):
+            m = re.match(r"http.*?:\/\/(.*?)(\/|$)", links[i])
+            lbaseurl = ''
+            if m:
+                lbaseurl = m.group(1)
+            if re.sub("\?.*$", "", links[i]) not in visited and baseurl in lbaseurl:
+                print(links[i])
+                with open(visitedfile, "a") as myfile:
+                    myfile.write(links[i]+"\n")
+                runRecursive(links[i],output)
 
 
 if len(sys.argv)>1:
     thisurl = sys.argv[1]
 else:
-    print('USAGE: ./url2corpus.py http://www.repubblica.it/esteri/2018/05/18/news/aereo_incidente_schianto_cuba_decollo-196760241/ ./corpus/ -r')
+    print('USAGE: ./url2corpus.py URL ./corpus/ -r')
+    print('Example URLS:\n http://www.repubblica.it/esteri/2018/05/18/news/aereo_incidente_schianto_cuba_decollo-196760241/\n https://www.ansa.it/sito/notizie/politica/2018/05/14/governo-di-maio-e-salvini-al-colle-nel-pomeriggio.-resta-nodo-premier_308ebf3c-4e34-4251-876c-9c9d83606e91.html\n http://www.repubblica.it/rss/homepage/rss2.0.xml\n http://www.ansa.it/sito/notizie/cronaca/cronaca_rss.xml')
     sys.exit()
 output = ""
 if len(sys.argv)>2 and os.path.isdir(sys.argv[2]):
     output = sys.argv[2]
 if len(sys.argv)>3:
     if sys.argv[3] == "-r" and output != "":
+        
+        visitedfile = output + "/visited.tmp"
+        if os.path.isfile(visitedfile):
+            visited = [line.rstrip('\n') for line in open(visitedfile)]
         #here we cycle for all the urls we can find
         print("I'm scanning the URL recursively looking for other pages to download. This is going to be endless (I mean it). When you are tired, just hit Ctrl+C.")
         runRecursive(thisurl, output)
